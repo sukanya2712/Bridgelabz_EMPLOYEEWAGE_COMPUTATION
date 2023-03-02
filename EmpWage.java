80bc9acb51b396537875d790df25565d2f71c387
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-03 00:04:20 +0530

    UC3_ADDED

--- EmpWage.java
+++ EmpWage.java
@@ -4,19 +4,26 @@ public class EmpWage {
     public static void main(String[] args) {
         System.out.println(" Welcome to employee wage");
 
-        int IS_PRESENT = 1;
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int EMP_WAGE_PER_HOUR = 20;
 
         int empHrs, empWage;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == IS_PRESENT){
-            System.out.println("Employee is Present");
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if(empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is Doing FUll Time.");
             empHrs = 8;
         }
+
+        else if (empCheck == IS_PART_TIME) {
+            System.out.println("Employee is Doing Part Time.");
+            empHrs = 4;
+        }
         else {
-            System.out.println("Employee is Absent");
+            System.out.println("Employee is Absent.");
             empHrs = 0;
         }
+
         empWage = empHrs * EMP_WAGE_PER_HOUR;
         System.out.println("Emp Hours : "+empHrs);
         System.out.println("Emp Wage : "+empWage);
