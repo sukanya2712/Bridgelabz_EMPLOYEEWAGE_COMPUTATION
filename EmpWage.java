c1a21fcdc4ebc860cf236eda3a86fd6f18a68305
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-03 00:00:25 +0530

    UC2_ADDED

--- EmpWage.java
+++ EmpWage.java
@@ -2,14 +2,23 @@ package com.bridgelabz;
 
 public class EmpWage {
     public static void main(String[] args) {
-        System.out.println(" Check whether Employee is present or absent");
-        int empPresesent = 1;
+        System.out.println(" Welcome to employee wage");
+
+        int IS_PRESENT = 1;
+        int EMP_WAGE_PER_HOUR = 20;
+
+        int empHrs, empWage;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == empPresesent){
+        if(empCheck == IS_PRESENT){
             System.out.println("Employee is Present");
+            empHrs = 8;
         }
         else {
             System.out.println("Employee is Absent");
+            empHrs = 0;
         }
+        empWage = empHrs * EMP_WAGE_PER_HOUR;
+        System.out.println("Emp Hours : "+empHrs);
+        System.out.println("Emp Wage : "+empWage);
     }
 }
